5c49e28bd8a2abf63cd38cb51445de6396b2e148
emre <yemreduymaz@hotmail.com>
2021-03-14 16:07:02 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _prodcutDal;                                // Şu Demek Sen bana IPRoductDal referansı var. Yarın Hibernate de olabilir, EntityFramework'te olabilir, InMemory de olabilir demek
+
+        public ProductManager(IProductDal prodcutDal)
+        {
+            _prodcutDal = prodcutDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _prodcutDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,72 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+
+            // Fake Data yaptık. Sanki Oracle, Sql Server, Postgres, MongoDB den geliyormuş gibi
+            _products = new List<Product> { 
+            
+                new Product{ ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15 },
+                new Product{ ProductId=2, CategoryId=1, ProductName="Kamere", UnitPrice=500, UnitsInStock=3 },
+                new Product{ ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2 },
+                new Product{ ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65 },
+                new Product{ ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1 },
+
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+        // LINQ - LANGUAGE INTEGRATED QUERY - Foreach kullanmamıza gerek kaldırmıyor. SingleOrDefault = Bir tane olan aramalarda kullanırız. Genelde Id eşitliğinde kullanırız. Alternatifleri( First )
+           Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+          /*
+            Product productToDelete = null;
+            foreach (var p in _products)
+            {
+                if(product.ProductId == p.ProductId)
+                {
+                    productToDelete = p;
+                }
+            }
+          */
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            //LINQ WHERE YENİ BİR LİSTE HALİNE GETİRİP ONU DÖNDÜRÜR
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+    }
+}
