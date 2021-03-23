e1bc517f1e8231828a956e28f4007a98b81a4b8e
emre <yemreduymaz@hotmail.com>
2021-03-23 20:58:26 +0300

    23 Mart ders yapıldı

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -4,6 +4,7 @@ using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcens.Validation;
 using Core.Utilities.Abstract.Results;
+using Core.Utilities.Business;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete;
 using DataAccess.Abstract;
@@ -11,26 +12,41 @@ using Entities.Concrete;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
+        //Bir Servise sadece kendinin dalını alabilir
         IProductDal _prodcutDal;                                // Şu Demek Sen bana IPRoductDal referansı var. Yarın Hibernate de olabilir, EntityFramework'te olabilir, InMemory de olabilir demek
+        ICategoryService _categoryService;                      // Micro Servis örneğidir.
 
-        public ProductManager(IProductDal prodcutDal)
+        public ProductManager(IProductDal prodcutDal, ICategoryService categoryService)
         {
             _prodcutDal = prodcutDal;
+            _categoryService = categoryService;
         }
 
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
+            // Pollymorhizm'e iyi bir örnek
+           IResult result =  BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId),
+               CheckIfProductNameExist(product.ProductName),
+               CheckIfCategoryLimitExceded());
+
+            if (result!=null)
+            {
+                return result;
+            }
+
             _prodcutDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
+            
         }
 
         public IResult AddTransactionalTest(Product product)
@@ -68,9 +84,48 @@ namespace Business.Concrete
             throw new NotImplementedException();
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Update(Product product)
         {
             throw new NotImplementedException();
         }
+
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            var result = _prodcutDal.GetAll(p => p.CategoryId == categoryId).Count;
+
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+
+            return new SuccessResult();
+        }
+        private IResult CheckIfProductNameExist(string productName)
+        {
+            var result = _prodcutDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+
+            return new SuccessResult();
+
+        }
+
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+
+            if (result.Data.Count>15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+
+            return new SuccessResult();
+
+        }
+
+
     }
 }
---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,23 @@
+﻿using Core.Utilities.Abstract.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        // Run içerisine istediğimiz kadar IResult verebiliyoruz parametre olarak
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;   // Hatalı logic geri döndürüyor. Oda logiğin içindeki error çalıştırır
+                }
+            }
+            return null;
+        }
+    }
+}
