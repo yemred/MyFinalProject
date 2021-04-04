c93b6220d64475c6ae32d466acbfdce4462f68c8
emre <yemreduymaz@hotmail.com>
2021-04-04 15:12:56 +0300

    4 Nisan Değişiklikler Yapıldı

--- Core/Extensions/ErrorDetails.cs
+++ Core/Extensions/ErrorDetails.cs
@@ -1,6 +1,5 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Extensions
@@ -9,7 +8,7 @@ namespace Core.Extensions
     {
         public string Message { get; set; }
         public int StatusCode { get; set; }
-
+     
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
